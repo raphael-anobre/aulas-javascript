7b57f2ef395bc03d39d131ee059605b560a2c84c
raphael-anobre <raphael_anobre@hotmail.com>
2023-04-06 17:22:01 -0300

    Algoritimo de um chatbot

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -84,5 +84,38 @@ let resultado = idade >= 18 ? "maior" : "menor";
 
 console.log(`A pessoa é ${resultado} de idade`);
 
-console.log();
+console.log("------------");
+
+/* switch/case/break/default */
+
+/* Algoritimo de um chatbot 
+Opções:
+1  -> Pedir informações
+2  -> Abrir uma reclamação
+3  -> Fazer um elogio
+X  -> Falar com atendente humano */
+let opcao = parseInt(prompt("Digite 1,2 ou 3"));
+let mensagem;
+
+switch(opcao){
+    case 1: mensagem = "ok, o que quer saber?";
+    break; 
+    
+    case 2:
+        mensagem = "Puxa, que pena. O que houve?";
+        break;
+
+        case 3: 
+        mensagem = "Bacana! Conte-me :)";
+        break;
+
+        default:
+            mensagem = "Não entendi... vou chamar alguém!";
+            break;
+}
+
+console.log(`Opção escolhida: ${opcao}`);
+console.log(mensagem);
+
+
 
