61cf985dae72327474b8aa6485d709b55d5a0fb5
raphael-anobre <raphael_anobre@hotmail.com>
2023-04-06 15:29:43 -0300

    Condicionais if / else

---
+++ js/05-condicionais.js
@@ -0,0 +1,38 @@
+/*Comandos condicionais
+if (se) / else (SENÃO)
+else if (SENÃO SE)
+
+switch (ESCOLHA/ANALISE) / case (CASO)
+
+Operadores relacionais:
+> (MAIOR QUE)
+< (MENOR QUE)
+>= (MAIOR OU IGUAL) 
+<= (MENOR OU IGUAL)
+== (IGUALDADE)
+!= (DIFERENTE) */
+
+// Exemplos
+const aluno = "Chaves"
+let nota1 = 10;
+let nota2 = 5;
+let faltas = 7;
+let media = (nota1 + nota2) /2;
+
+console.log(`Aluno(a): ${aluno}`);
+console.log(`Notas: ${nota1} e ${nota2}`);
+console.log(`Média: ${media}`);
+
+// Critério de aprovação
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+/* ALGORITIMO: Se a média obtida pelo aluno(a)
+for maior ou igual à média mínima da escola E se suas faltas forem abaixo do limite, então ele estará aprovado. Caso contrário,
+esle estará reprovado. */
+if(media >= mediaMinima && faltas < limiteDeFaltas){
+    console.log("aprovado");
+} else {
+    console.log("reprovado");
+}
+
